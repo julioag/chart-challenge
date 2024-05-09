e2bf2098985b73e05e708629a10fe8d2fa6bb6a9
julioag <jnandrade@uc.cl>
2024-05-09 11:54:18 -0400

    chore: set env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import Chart from "../ui/chart";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyArGiRgGd2MfE65_9sjE2QX49gt1sP0GmA",
-  authDomain: "racional-exam.firebaseapp.com",
-  databaseURL: "https://racional-exam.firebaseio.com",
-  projectId: "racional-exam",
-  storageBucket: "racional-exam.appspot.com",
-  messagingSenderId: "669314004725",
-  appId: "1:669314004725:web:48bd14a97d7db43c91f7bc"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../lib/database";
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -30,7 +17,6 @@ export default function Home() {
         console.log("No such document!");
       }
     });
-    // Limpiar el oyente al desmontar el componente
     return () => unsubscribe();
   }, []);
 
